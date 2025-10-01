91e73d29394a375bac1eeb7441b906455baa7021
SpikeIreland <paul@spikeisland.tv>
2025-10-01 08:13:24 +0800

    Create simple phases page to test routing

---
+++ app/phases/page.tsx
@@ -0,0 +1,8 @@
+export default function PhasesPage() {
+  return (
+    <div style={{ padding: '50px' }}>
+      <h1>6-Phase Negotiation Process</h1>
+      <p>This is a test page. If you can see this, the routing works.</p>
+    </div>
+  )
+}
